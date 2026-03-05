5e5e74c2577971a0413e160587dee646267ee3d4
Qusai-Nazzal <s12324944@stu.najah.edu>
2026-03-05 04:42:49 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: 'https://www.google.com' }}
+          style={styles.webview}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
